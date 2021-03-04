ed3845725c168a92119ed81b44d0e109e793996b
makhatadze <vitomaxatadze@gmail.com>
2021-03-04 15:45:27 +0400

    Add mediator users_permissions table.

--- database/migrations/2021_03_04_114329_create_users_permissions_table.php
+++ database/migrations/2021_03_04_114329_create_users_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersRolesTable extends Migration
+class CreateUsersPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersRolesTable extends Migration
      */
     public function up()
     {
-        Schema::create('users_roles', function (Blueprint $table) {
+        Schema::create('users_permissions', function (Blueprint $table) {
             $table->bigInteger('user_id')->unsigned()->index();
-            $table->bigInteger('role_id')->unsigned();
+            $table->bigInteger('permission_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-            $table->primary(['user_id', 'role_id']);
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->primary(['user_id','permission_id']);
 
             // Add columns to find author for this action.
             $table->foreignId('created_by')->unsigned()->nullable()->constrained('users')->onDelete('set null');
@@ -33,6 +33,6 @@ class CreateUsersRolesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_roles');
+        Schema::dropIfExists('users_permissions');
     }
 }
