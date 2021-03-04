8a776a4b8fff913aed6ab850c0e364bc02f5b49d
makhatadze <vitomaxatadze@gmail.com>
2021-03-04 14:26:27 +0400

    Add roles model and migration

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ *  app/Models/Role.php
+ *
+ * Date-Time: 04.03.21
+ * Time: 14:26
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
--- database/migrations/2021_03_04_102158_create_roles_table.php
+++ database/migrations/2021_03_04_102158_create_roles_table.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ *  database/migrations/2021_03_04_102158_create_roles_table.php
+ *
+ * Date-Time: 04.03.21
+ * Time: 14:26
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
