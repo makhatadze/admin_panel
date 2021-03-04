227e16d2d4d65e041e1b7703862f23c18601db3b
makhatadze <vitomaxatadze@gmail.com>
2021-03-04 15:35:30 +0400

    Add routes model with migration.

---
+++ app/Models/Route.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *  app/Models/Route.php
+ *
+ * Date-Time: 04.03.21
+ * Time: 15:33
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Route extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_03_04_113317_create_routes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoutesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreignId('created_by')->unsigned()->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('updated_by')->unsigned()->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('deleted_by')->unsigned()->nullable()->constrained('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routes');
+    }
+}
